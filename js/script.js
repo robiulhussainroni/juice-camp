dc1b21e405458b8bee2973b86f7349ff712b734a
Roni <rhroni09@gmail.com>
2024-06-03 16:53:09 +0600

    added mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+// Getting new year
 const year = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 year.textContent = currentYear;
+
+// Making Mobile Navigation Work
+const mobileNavEl = document.querySelector(".mobile-nav");
+const headerEl = document.querySelector(".main-header");
+
+mobileNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
