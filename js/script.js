30597ca5e2052cbecd4ad425722d85c8e6d8471e
Roni <rhroni09@gmail.com>
2024-06-11 20:45:54 +0600

    updated mobile navigation

--- js/script.js
+++ js/script.js
@@ -14,32 +14,11 @@ btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
-///////////////////////////////////////////////////////////
-// Smooth scrolling animation
+const navLinks = document.querySelectorAll(".main-nav-link");
 
-const allLinks = document.querySelectorAll("a:link");
-
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (e) {
-    e.preventDefault();
-    const href = link.getAttribute("href");
-
-    // Scroll back to top
-    if (href === "#")
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-
-    // Scroll to other links
-    if (href !== "#" && href.startsWith("#")) {
-      const sectionEl = document.querySelector(href);
-      sectionEl.scrollIntoView({ behavior: "smooth" });
-    }
-
-    // Close mobile naviagtion
-    if (link.classList.contains("main-nav-link"))
-      headerEl.classList.toggle("nav-open");
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    headerEl.classList.remove("nav-open");
   });
 });
 
